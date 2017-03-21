633c4e1c497cc46a8b0b2dea1707fc2ad85a3ecf
danthelion <danivgy@gmail.com>
2017-03-21 12:19:32 +0100

    remove long desc

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 from setuptools import setup
 
-with open("README.md", 'r') as f:
-    long_description = f.read()
-
 setup(
     name='pyet',
     version='1.0',
     author='Daniel Palma',
     license="MIT",
-    long_description=long_description,
     author_email='danivgy@gmail.com',
     url='https://github.com/danthelion/pyet',
     scripts=['pyet/pyet.py'],
